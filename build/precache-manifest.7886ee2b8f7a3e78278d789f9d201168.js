af76807107b555091fc6d2f3170c1742ebcbf272
Prashant Nigam <prashant.nigam@gmail.com>
2022-02-27 18:58:43 -0500

    ran npm run build
    ran npm run build

---
+++ build/precache-manifest.7886ee2b8f7a3e78278d789f9d201168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba174411dd5f6b0cb9f7d58a1735a63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "529934ff05bf114b1412",
+    "url": "/static/css/main.74fb3d2a.chunk.css"
+  },
+  {
+    "revision": "cc8efbf0ba7a4ba49147",
+    "url": "/static/js/2.9b50b19e.chunk.js"
+  },
+  {
+    "revision": "529934ff05bf114b1412",
+    "url": "/static/js/main.2a47446d.chunk.js"
+  },
+  {
+    "revision": "b6135ab51b693019ac38",
+    "url": "/static/js/runtime-main.801b7f43.js"
+  },
+  {
+    "revision": "f84cc483cab9c1a320bdbc3bcda2ee97",
+    "url": "/static/media/polygonlogo.f84cc483.png"
+  },
+  {
+    "revision": "8cea1391b8e0717ea6532ebfa5cf4a34",
+    "url": "/static/media/twitter-logo.8cea1391.svg"
+  }
+]);
